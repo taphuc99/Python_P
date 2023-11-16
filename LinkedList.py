3110f8602861d3422c7d70cbd8f45891ffab13f2
taphuc99 <supernoah.50@gmail.com>
2023-11-16 09:46:33 +0700

    Linked List

---
+++ LinkedList.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print(printval.dataval)
+            printval = printval.nextval
+
+    def AtBegining(self, newdata):
+        NewNode = Node(newdata)
+        NewNode.nextval = self.headval
+        self.headval = NewNode
+
+    def AtEnd(self, newdata):
+        NewNode = Node(newdata)
+        if self.headval is None:
+            self.headval = NewNode
+            return
+        laste = self.headval
+        while laste.nextval:
+            laste = laste.nextval
+        laste.nextval = NewNode
+
+    def Inbetween(self, middle_node, newdata):
+        if middle_node is None:
+            print("The mentioned node is absent")
+            return
+
+        NewNode = Node(newdata)
+        NewNode.nextval = middle_node.nextval
+        middle_node.nextval = NewNode
+
+    # Function to remove node
+    def RemoveNode(self, Removekey):
+        HeadVal = self.head
+            
+        if (HeadVal is not None):
+            if (HeadVal.data == Removekey):
+                self.head = HeadVal.next
+                HeadVal = None
+                return
+        while (HeadVal is not None):
+            if HeadVal.data == Removekey:
+                break
+            prev = HeadVal
+            HeadVal = HeadVal.next
+
+        if (HeadVal == None):
+            return
+
+        prev.next = HeadVal.next
+        HeadVal = None
+
+        
+list = LinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+# Link first Node to second Node
+list.headval.nextval = e2
+# Link second Node to third Node
+e2.nextval = e3
+
+list.AtBegining("Sun")
+list.AtEnd("Thu")
+list.Inbetween(list.headval.nextval,"Fri")
+list.listprint()
